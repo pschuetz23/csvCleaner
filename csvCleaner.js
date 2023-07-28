9ac4655cd3f606aaa7c122a1ad7aa22f1b5cd8b7
pschuetz23 <140216476+pschuetz23@users.noreply.github.com>
2023-07-28 11:15:34 -0700

    Update csvCleaner.js
    Cleaned formatting and restructured functions.

--- csvCleaner.js
+++ csvCleaner.js
@@ -1,19 +1,26 @@
-//import required modules
+/**
+ * csvCleaner - script to clean CSV files by normalizing all non-latin characters
+ *  and stripping special characters.
+ *
+ * Receives a CSV file and outputs a cleaned version with a new name.
+ */
 
-import { parse, stringify } from '@vanillaes/csv'
-import { argv } from 'node:process';
-import * as fs from 'fs';
+import { parse, stringify } from "@vanillaes/csv";
+import { argv } from "node:process";
+import * as fs from "fs";
 
 //filter inputs
 
 if (process.argv.length != 3) {
-    console.error('Invalid arguments! You only need your CSV file as input.');
-    process.exit(1);
-  }
+  console.error("Invalid arguments! You only need your CSV file as input.");
+  process.exit(1);
+}
 
-console.warn("Remember to match headers to form after file is cleaned to ensure it uploads correctly.");
+console.warn(
+  "Remember to match headers to form after file is cleaned to ensure it uploads correctly."
+);
 
-console.info("Cleaning will take ~30 sec.");  
+console.info("Cleaning will take ~30 sec.");
 
 //receive and parse CSV file
 
@@ -21,76 +28,51 @@ const inputTable = parse(fs.readFileSync(process.argv[2]));
 
 //if acceptable includes table[i] {append table[i] to write}
 
-let myarray = [];
-const regex = /[^A-Za-z0-9]/g;
+let myArray = [];
+const regexEmail = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi;
+const regexPhone = /[+]+[\d]/g;
 
-function evalArray() {
+function cleanString(inputString) {
+  //remove all accented characters and replace other oddities with nothing
+  let noSpecial = inputString.normalize("NFD").replace(/[\u007b-\u036f]/g, "");
 
-    inputTable.forEach(item => {
+  if (regexEmail.test(noSpecial)) return noSpecial;
+  if (regexPhone.test(noSpecial)) return noSpecial;
 
-      //array to store filtered strings
-      const arr = [];
+  //cleaning improper formatting e.g. surc\work -> surc/work
+  noSpecial = noSpecial.replace(/[\\]/g, "/");
 
-      item.forEach(it => {
+  //remove all non-accepted special characters (accept "'-&,/")
+  noSpecial = noSpecial.replace(/[^A-Za-z0-9 '\-,&/]/g, "");
 
-        //cleanup regexs
+  //remove any special that precedes blank space, attempt to remove extra specials
+  noSpecial.replace(/[^A-Za-z0-9]+[" "]/g, "");
 
-        //remove all accented characters and replace other oddities with nothing
-        let noSpecial = it.normalize("NFD").replace(/[\u007b-\u036f]/g, "");
+  //remove repeating special characters (if any)
+  noSpecial = noSpecial.replace(/[^A-Za-z0-9 ]{2,}/g, "");
 
-        //remove all non-accepted special characters (accept "+&@'.-_/\,")
-        noSpecial = noSpecial.replace(/[^\w\s+&@\'.\-\/\\,]|[\_]/g, "");
+  //remove any double spaces
+  noSpecial = noSpecial.replace(/[" "]+[" "]/g, " ");
 
-        //remove any special that precedes blankspace, attempt to remove extra specials
-        noSpecial.replace(/[^A-Za-z0-9]+[" "]/g, "");
-
-        //if field is an email, do nothing
-        if(/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi.test(noSpecial)) {
-          arr.push(noSpecial);
-
-        } else {            
-          //regex to remove "+" sign, but ignore if it's in phone field
-
-          if(/[+]+[\d]/g.test(noSpecial)) {
-            arr.push(noSpecial);
-
-          } else {
-            //more aggressive regex
-            //cleaning improper formatting e.g. surc\work -> surc/work
-            noSpecial = noSpecial.replace(/[\\]/g, "/")
-            
-            //remove all non-accepted special characters (accept "'-&,/")
-            noSpecial = noSpecial.replace(/[^A-Za-z0-9 '\-,&/]/g, "");
-              
-            //remove repeating special characters (if any)
-            noSpecial = noSpecial.replace(/[^A-Za-z0-9 ]{2,}/g, "");
-  
-            //remove any double spaces
-            noSpecial = noSpecial.replace(/[" "]+[" "]/g, " ");
-  
-            //append to filter array
-            arr.push(noSpecial);
-          }
-        }
-      })
-        
-      myarray.push(arr);
-        
-   })
+  return noSpecial;
 }
 
-//main method
+//function to increment through input table, clean strings, then output to 2d array
+function evalArray() {
+  inputTable.forEach((item) => {
+    const arr = [];
+
+    item.forEach((it) => arr.push(cleanString(it)));
+    myArray.push(arr);
+  });
+}
 
 evalArray();
 
 //converting filtered array back into CSV format
-const out = stringify(myarray);
+const out = stringify(myArray);
 
-//write to file and catch errors
-fs.writeFile('cleanedCSV.csv', out, (err) => {
+fs.writeFile("cleanedCSV.csv", out, (err) => {
   if (err) throw err;
-  else {
-    console.log("File created sucessfully!")
-  }
-})
-
+  console.log("File created successfully!");
+});
